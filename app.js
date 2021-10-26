611300093d1cae4e5138446dd58d19a210feb78f
boxer <aszzoon@naver.com>
2021-10-26 10:56:50 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const loginInput = document.querySelector("#login-form input");
+const loginbutton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  console.log(loginInput.value);
+}
+
+loginbutton.addEventListener("click", onLoginBtnClick);
