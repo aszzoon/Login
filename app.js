1e9a46e65e6117869b96ec2942097cc23a2c6ce9
boxer <aszzoon@naver.com>
2021-10-26 15:52:39 +0900

    Events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginbutton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-  const username = loginInput.value;
-  console.log(username);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
 }
 
-loginbutton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
